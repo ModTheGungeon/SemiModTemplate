bf2aa3866d609071b650241d8415afe03b46ad0d
Zatherz <zatherz@linux.pl>
2019-08-27 00:30:05 +0200

    Initial commit

---
+++ src/ExampleMod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Semi;
+using Logger = ModTheGungeon.Logger;
+
+namespace SemiModTemplate {
+	public class ExampleMod : Mod {
+		public Logger Logger = new Logger("ExampleMod");
+
+		public override void Loaded() {
+			Logger.Info($"Hello world!");
+		}
+
+		public override void RegisterContent() {
+			Logger.Debug($"Registering content.");
+		}
+
+		public override void InitializeContent() {
+			Logger.Debug($"Initializing content.");
+		}
+	}
+}
